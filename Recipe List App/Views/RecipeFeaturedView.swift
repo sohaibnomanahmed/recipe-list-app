c45d1ff0a47ad31b3936fdaf4f31112201c780ab
Sohaib Ahmed <sohaib.noman.ahmed@gmail.com>
2022-08-15 12:13:55 +0200

    Build UI in the featured view

--- Recipe List App/Views/RecipeFeaturedView.swift
+++ Recipe List App/Views/RecipeFeaturedView.swift
@@ -8,13 +8,57 @@
 import SwiftUI
 
 struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model:RecipeModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 0) {
+            Text("Featured Recipe")
+                .padding(.leading)
+                .font(.largeTitle)
+            GeometryReader { geo in
+                TabView {
+                    ForEach(0..<model.recipies.count, id: \.self){ index in
+                        if model.recipies[index].featured {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.white)
+                            
+                                VStack(spacing: 0){
+                                    Image(model.recipies[index].image)
+                                        .resizable()
+                                        .clipped()
+                                        .aspectRatio(contentMode: .fill)
+                                    Text(model.recipies[index].name)
+                                        .padding()
+                                }
+                            }
+                            .frame(width: geo.size.width - 40 , height: geo.size.height - 120, alignment: .center)
+                            .cornerRadius(15)
+                            .shadow(radius: 10)
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            VStack(alignment: .leading, spacing: 5){
+                Text("Preperation Time:")
+                    .font(.headline)
+                Text("1 Hour")
+                Text("Highlights")
+                    .font(.headline)
+                Text("Featured Recipe")
+            }
+            .padding(.leading)
+        }
     }
 }
 
 struct RecipeFeaturedView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeaturedView()
+            .environmentObject(RecipeModel())
     }
 }
